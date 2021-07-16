87dff0bb8acb58471f1926245f37e0188f4062f9
Wellynton Wudke <tomwudke@gmail.com>
2021-07-16 09:38:15 -0300

    Alterações de erro (16/07/2021)

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,7 @@
 import React, { useState, FormEvent } from 'react';
 import api from '../../services/api';
 
-import { FiChevronRight } from 'react-icons/fi';
-
-import { Title, Repositories, Form } from './styles';
+import { Title, Repositories, Form, Error } from './styles';
 
 interface Repository{
   full_name: string;
@@ -16,23 +14,35 @@ interface Repository{
 
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
   const [repositories, setRepositories] = useState<Repository[]>([]);
 
   async function handleAddRepository(event: FormEvent<HTMLFormElement>,): Promise<void> {
     event.preventDefault();
 
-    const response = await api.get(`repos/${newRepo}`);
-    const repository = response.data;
+    if(!newRepo){
+        setInputError("Digite um usuário/repositório para pesquisar.");
+        return;
+    }
 
-    setRepositories([...repositories, repository]);
-    setNewRepo('');
+    try {
+      const response = await api.get(`repos/${newRepo}`);
+      const repository = response.data;
+
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+
+    } catch (err) {
+      setInputError ("Repositório não encontrado ou inexistente")
+    }
   };
 
   return (
     <>
     <Title>Explore repositórios no Github</Title>
 
-  <Form onSubmit={handleAddRepository}>
+  <Form hasError={ !!inputError } onSubmit={handleAddRepository}>
     <input
     value={newRepo}
     onChange={e => setNewRepo(e.target.value)}
@@ -40,21 +50,11 @@ const Dashboard: React.FC = () => {
       <button type="submit">Pesquisar</button>
   </Form>
 
-    <Repositories>
-                  <a href="teste">
-                    <img
-                        src="https://avatars.githubusercontent.com/u/80406057?v=4"
-                        alt="WellZ3n"
-                    />
-                    <div>
-                        <strong>Git-Repo</strong>
-                        <p>Meu primeiro projeto em React (05/07/2021)</p>
-                    </div>
-                    <FiChevronRight size={25} />
-                  </a>
+    {inputError && <Error>{inputError}</Error>}
 
-    {/**repositories.map(repository => (
-                    <a>
+    <Repositories>
+    {repositories.map(repository => (
+                    <a key={repository.full_name} href="teste">
                     <img
                         src={repository.owner.avatar_url}
                         alt={repository.owner.login}
@@ -64,18 +64,7 @@ const Dashboard: React.FC = () => {
                         <p>{repository.description}</p>
                     </div>
                     </a>
-    ))**/}
-                    <a href="teste">
-                    <img
-                        src="https://avatars.githubusercontent.com/u/46034178?v=4"
-                        alt="Johnatan"
-                    />
-                     <div>
-                        <strong>api_logistica</strong>
-                        <p>API desenvolvida em Java com Spring Boot, Spring Web, Spring Security, Spring Data, Lombok, Model Mapper, FlywayDB, JWT e PostgreSQL.</p>
-                     </div>
-                      <FiChevronRight size={50} />
-                    </a>
+    ))}
       </Repositories>
      </>
     );
